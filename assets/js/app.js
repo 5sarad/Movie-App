2714df81c7bd02ac1da4622db4bfe009a238a4e4
5sarad <saradpoudel05@gmail.com>
2024-03-19 21:24:35 +1100

    executed codes in JS.app

---
+++ assets/js/app.js
@@ -0,0 +1,64 @@
+class Movie {
+    constructor(title, year, released, genre, director, writer, plot, poster) {
+        this.title = title;
+        this.year = year;
+        this.released = released;
+        this.genre = genre;
+        this.director = director;
+        this.writer = writer;
+        this.plot = plot;
+        this.poster = poster;
+    }
+}
+
+class MovieApp {
+    constructor() {
+        this.searchQuery = "";
+        this.resultMovie = null;
+        this.recentMovies = [];
+        this.activeMovies = null;
+    }
+
+    async searchMovie(event) {
+        if (event.code === "Enter") {
+            event.preventDefault();
+            const searchInput = document.querySelector("input");
+            const searchQuery = searchInput.value;
+            const apiEndPoint = `http://www.omdbapi.com/?t=${searchQuery}&apikey=45112140`;
+            const response = await fetch(apiEndPoint);
+            const result = await response.json();
+            if (result) {
+                this.resultMovie = new Movie(
+                    result.Title,
+                    result.Year,
+                    result.Released,
+                    result.Genre,
+                    result.Director,
+                    result.Writer,
+                    result.Plot,
+                    result.Poster
+                );
+                console.log(this.resultMovie);
+            }
+        }
+    }
+
+    attachSearchEventListener() {
+        const searchInput = document.querySelector("input");
+        searchInput.addEventListener("keydown", this.searchMovie);
+    }
+}
+
+const movieObject = new Movie(
+    "Guardians of the Galaxy Vol. 2",
+    "2017",
+    "05 May 2017",
+    "Action, Adventure, Comedy",
+    "James Gunn",
+    "James Gunn, Dan Abnett, Andy Lanning",
+    "The Guardians struggle to keep together as a team while dealing with their personal family issues, notably Star-Lord's encounter with his father, the ambitious celestial being Ego.",
+    "poster-url"
+);
+
+const movieApp = new MovieApp();
+movieApp.attachSearchEventListener();
